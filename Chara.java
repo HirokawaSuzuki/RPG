060d4263c4e023ac5147c55c3afae32f40087cd1
Masa <masanobu827@gmail.com>
2025-08-10 22:55:00 +0900

    初回コミット

---
+++ Chara.java
@@ -0,0 +1,67 @@
+package RPG;
+import java.util.Scanner;
+
+public class Chara {
+    private String name;
+    private int hp;
+    private int atk;
+    private int def;
+    public String text;
+    public int num;
+
+    
+    public static void waitForEnter() {
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        // scannerはここで閉じない（System.inが閉じられるため）
+    }
+
+    public Chara(String name, int hp, int atk, int def) {
+        this.name = name;
+        this.hp = hp;
+        this.atk = atk;
+        this.def = def;
+    }
+
+    public void speak(String text){
+        System.out.println(this.name + ":" + text);
+        Chara.waitForEnter();
+    }
+    public int getHP() {
+        return this.hp;
+    }
+    public void hp(){
+        System.out.println(this.name + "HP:" + this.hp);
+    }
+    
+    public int choice(int num){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1.たたかう 2.ぼうぎょ 3.にげる");
+        speak("おれは...");
+        num = scanner.nextInt();
+        switch(num){
+            case 1:
+                speak("たたかう！");
+                break;
+            case 2:
+                speak("ぼうぎょだ！\n" + this.name + "の防御:");
+                break;
+            case 3:
+                speak()
+        }
+    }
+
+    public void attack(Chara target){
+        int damage = this.atk - target.def;
+        if (damage < 0){
+            damage = 0;
+        }
+        target.hp -= damage;
+        if(target.hp <= 0){
+            target.speak(target.name + "はたおれた！\nせんとうはおわりだ！");
+        }else{
+            speak(this.name + " は " + target.name + " にこうげき！");
+            speak("ダメージは " + damage);
+        }
+    }
+}
---
+++ play.java
@@ -0,0 +1,31 @@
+package RPG;
+
+public class play {
+    public static void main(String[] arge){
+        int p_hp = 8;
+        int m_hp =10;
+
+        Chara hero = new Chara("けんたろ",p_hp,3,1);
+        Chara monster = new Chara("スライム",m_hp,5,2);
+        hero.speak("ぼうけんを始めよう！");
+        hero.speak("お？スライムだ！");
+        monster.speak("スライムは襲い掛かってきた！");
+        
+
+        while(p_hp >= 0 && m_hp >= 0){
+            p_hp = hero.getHP();
+            m_hp = monster.getHP();
+            hero.hp();
+            monster.hp();
+            hero.speak("どうする？");
+            hero.attack(monster);
+            if(m_hp <= 0){
+                monster.speak("スライムは倒れた！");
+                monster.speak("戦闘終了！");
+                break;
+            }
+            monster.attack(hero);
+
+        }
+    }
+}
