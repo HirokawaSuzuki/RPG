b061f1a3ed730c5b4680cc56237606a2cf34815a
Masa <masanobu827@gmail.com>
2025-08-13 00:34:39 +0900

    選択肢ごとに行動を変えられるchoice()を追加 main()を簡素化した

--- play.java
+++ play.java
@@ -1,29 +1,11 @@
 public class play {
     public static void main(String[] arge){
-        int p_hp = 8;
-        int m_hp =10;
-
-        Chara hero = new Chara("けんたろ",p_hp,3,1);
-        Chara monster = new Chara("スライム",m_hp,5,2);
-        hero.speak("ぼうけんを始めよう！");
-        hero.speak("お？スライムだ！");
+        hero kentaro = new hero("けんたろ",8,3,1);
+        Chara monster = new Chara("スライム",10,5,2);
+        kentaro.speak("ぼうけんを始めよう！");
+        kentaro.speak("お？スライムだ！");
         monster.speak("スライムは襲い掛かってきた！");
-        
 
-        while(p_hp >= 0 && m_hp >= 0){
-            p_hp = hero.getHP();
-            m_hp = monster.getHP();
-            hero.hp();
-            monster.hp();
-            hero.speak("どうする？");
-            hero.attack(monster);
-            if(m_hp <= 0){
-                monster.speak("スライムは倒れた！");
-                monster.speak("戦闘終了！");
-                break;
-            }
-            monster.attack(hero);
-
-        }
+        kentaro.battle(monster);
     }
 }
